20ea36e459cf7213ffee0eb2d947286a306c4511
KoVAcomm <146743647+KoVAcomm@users.noreply.github.com>
2023-10-06 19:48:05 +0500

    schetchik points

--- Ping_game.py
+++ Ping_game.py
@@ -1,6 +1,9 @@
 import pygame
 import random
 pygame.init()
+pygame.font.init()
+sch_1 = 0
+sch_2 = 0
 class Spirt(pygame.sprite.Sprite):
     def __init__(self, filename, width, height, x, y):
         self.filename = filename
@@ -34,7 +37,10 @@ class ball(Player):
     def __init__(self, filename, width, height, x, y, speed, speed_1):
         super().__init__(filename, width, height, x, y, speed)
         self.speed_1 = speed_1
+        
     def wasd_2(self):
+        global sch_1
+        global sch_2
         if flag_start:
             a = random.randint(0, 1)
             if a == 0:
@@ -46,26 +52,32 @@ class ball(Player):
                 self.speed *= -1
                 self.speed_1*=1
         else:
-           
-            if self.rect.x <= 0 or self.rect.x >= 700:
-
-                self.speed *= -1
-                self.speed_1 *= 1
             
             if self.rect.y >= 450 or self.rect.y <= 0:
                 self.speed *= 1
                 self.speed_1 *= -1
+            if self.rect.x <= 0 or self.rect.x >= 700:
+
+                self.speed *= -1
+                self.speed_1 *= 1
+                if self.rect.x <= 0:
+                    sch_2 +=1
+                elif self.rect.x >= 700:
+                    sch_1 +=1
+            
+               
             
         self.rect.x += self.speed
         self.rect.y += self.speed_1
-
+        #if self.rect.x 
+fon_t = pygame.font.SysFont("Arial", 25)
 back = Spirt("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/p_back.png", 750, 500, 0, 0)
-player = Player("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/gamer_1.png", 25, 88, 35, 150, 10)
-player_1 = Player("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/gamer_2.png", 25, 88, 680, 150, 10)
-balli = ball("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/ball.png", 50, 50, 250, 300, 10, 10)
+player = Player("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/gamer_1.png", 25, 88, 35+30, 150, 10)
+player_1 = Player("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/gamer_2.png", 25, 88, 680-30, 150, 10)
+balli = ball("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/ball.png", 50, 50, 250, 300, 10, 6)
 clock = pygame.time.Clock()
 game = True
-flag_start = True
+
 win = pygame.display.set_mode((750, 500))
 pygame.display.set_caption("Ping: 9999")
 i = 0
@@ -79,18 +91,24 @@ while game:
         flag_start = False
     
 
-    balli.wasd_2()
+    
     
     player.reset()
     player.wasd()
     player_1.reset()
     player_1.wasd_1()
-    if pygame.sprite.collide_rect(balli, player) or pygame.sprite.collide_rect(balli, player_1):
+    
+    if pygame.sprite.collide_rect(player, balli) or pygame.sprite.collide_rect(player_1, balli):
         balli.speed *= -1
+        
         if (balli.rect.y ==  player.top or balli.rect.y ==  player.botton) or (balli.rect.y ==  player_1.top or balli.rect.y ==  player_1.botton):
             balli.speed_1*=-1
-
-
+           
+    
+    balli.wasd_2()
+    win.blit(fon_t.render(("Красный: "+str(sch_1)) , True, (255, 0, 100)), (230, 5))
+    win.blit(fon_t.render((" Vs. ") , True, (255, 255, 255)), (350, 5))
+    win.blit(fon_t.render(("Зелёный: "+str(sch_2)), True, (0, 255, 0)), (400, 5))
     i = 10
     pygame.display.update()
     clock.tick(60)
