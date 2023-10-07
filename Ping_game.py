663cf3bb1e07e132a340627329741efeec24bb56
KoVAcomm <146743647+KoVAcomm@users.noreply.github.com>
2023-10-07 17:32:14 +0500

    desaign and window for winner

--- Ping_game.py
+++ Ping_game.py
@@ -37,7 +37,6 @@ class ball(Player):
     def __init__(self, filename, width, height, x, y, speed, speed_1):
         super().__init__(filename, width, height, x, y, speed)
         self.speed_1 = speed_1
-        
     def wasd_2(self):
         global sch_1
         global sch_2
@@ -46,38 +45,37 @@ class ball(Player):
             if a == 0:
                 self.speed *= 1
                 self.speed_1 *= -1
-                
             elif a == 1:
-                
                 self.speed *= -1
                 self.speed_1*=1
         else:
-            
             if self.rect.y >= 450 or self.rect.y <= 0:
                 self.speed *= 1
                 self.speed_1 *= -1
             if self.rect.x <= 0 or self.rect.x >= 700:
-
                 self.speed *= -1
                 self.speed_1 *= 1
                 if self.rect.x <= 0:
                     sch_2 +=1
                 elif self.rect.x >= 700:
                     sch_1 +=1
-            
-               
-            
+                if sch_1 == 50 or sch_2 == 50:
+                    global win_n
+                    win_n = True
         self.rect.x += self.speed
         self.rect.y += self.speed_1
-        #if self.rect.x 
+pygame.display.set_icon(pygame.image.load("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/ping.bmp"))
 fon_t = pygame.font.SysFont("Arial", 25)
-back = Spirt("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/p_back.png", 750, 500, 0, 0)
-player = Player("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/gamer_1.png", 25, 88, 35+30, 150, 10)
-player_1 = Player("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/gamer_2.png", 25, 88, 680-30, 150, 10)
+fon_t_1 = pygame.font.SysFont("Arial", 50)
+back = Spirt("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/p_back.png", 750, 500, 0, 0) 
+player = Player("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/gamer_1.png", 25, 88, 35+30, 190, 10)
+player_1 = Player("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/gamer_2.png", 25, 88, 680-30, 190, 10)
 balli = ball("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/ball.png", 50, 50, 250, 300, 10, 6)
+table = Spirt("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/table.png", 375, 60, 180, 5)
+table_pob = Spirt("C:/Users/guest/OneDrive/Рабочий стол/Ping_999/table.png", 600, 96, 80, 180)
 clock = pygame.time.Clock()
 game = True
-
+win_n = False
 win = pygame.display.set_mode((750, 500))
 pygame.display.set_caption("Ping: 9999")
 i = 0
@@ -86,29 +84,32 @@ while game:
         if e.type == pygame.QUIT:
             game = False
     back.reset()
-    balli.reset()
-    if i <= 0:
-        flag_start = False
-    
-
-    
-    
-    player.reset()
-    player.wasd()
-    player_1.reset()
-    player_1.wasd_1()
-    
-    if pygame.sprite.collide_rect(player, balli) or pygame.sprite.collide_rect(player_1, balli):
-        balli.speed *= -1
-        
-        if (balli.rect.y ==  player.top or balli.rect.y ==  player.botton) or (balli.rect.y ==  player_1.top or balli.rect.y ==  player_1.botton):
-            balli.speed_1*=-1
-           
-    
-    balli.wasd_2()
-    win.blit(fon_t.render(("Красный: "+str(sch_1)) , True, (255, 0, 100)), (230, 5))
-    win.blit(fon_t.render((" Vs. ") , True, (255, 255, 255)), (350, 5))
-    win.blit(fon_t.render(("Зелёный: "+str(sch_2)), True, (0, 255, 0)), (400, 5))
-    i = 10
+    if win_n == False:
+        balli.reset()
+        if i <= 0:
+            flag_start = False
+        player.reset()
+        player.wasd()
+        player_1.reset()
+        player_1.wasd_1()
+        if pygame.sprite.collide_rect(player, balli) or pygame.sprite.collide_rect(player_1, balli):
+            balli.speed *= -1
+            if (balli.rect.y ==  player.top or balli.rect.y ==  player.botton) or (balli.rect.y ==  player_1.top or balli.rect.y ==  player_1.botton):
+                balli.speed_1*=-1
+        balli.wasd_2()
+        table.reset()
+        win.blit(fon_t.render(("Красный: "+str(sch_1)) , True, (255, 0, 0)), (225, 20))
+        win.blit(fon_t.render((" Vs. ") , True, (255, 255, 255)), (350, 20))
+        win.blit(fon_t.render(("Зелёный: "+str(sch_2)), True, (0, 255, 100)), (400, 20))
+        i = 10
+    else:
+        table_pob.reset()
+        if sch_1 > sch_2:
+            pob = fon_t_1.render("Победил Красный!", True, (255, 0, 0))
+        elif sch_1 < sch_2:
+            pob = fon_t_1.render("Победил Зелёный!", True, (0, 255, 100))
+        else:
+            pob = fon_t_1.render("Ничья", True, (255, 255, 255))
+        win.blit(pob, (180, 200))
     pygame.display.update()
     clock.tick(60)
